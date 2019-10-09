461c5f0ad948f8e610e1e2a369d0bb68c8bea534
98762 <987625922@qq.com>
2019-10-09 22:07:16 +0800

    更新项目

--- src/main/java/com/wind/springbootlearn2/Springbootlearn2Application.java
+++ src/main/java/com/wind/springbootlearn2/Springbootlearn2Application.java
@@ -2,6 +2,10 @@ package com.wind.springbootlearn2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class Springbootlearn2Application {
@@ -10,4 +14,12 @@ public class Springbootlearn2Application {
         SpringApplication.run(Springbootlearn2Application.class, args);
     }
 
+    //对MultipartFile文件上传的大小进行配置
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("10240KB");
+        factory.setMaxRequestSize("1024000KB");
+        return factory.createMultipartConfig();
+    }
 }
