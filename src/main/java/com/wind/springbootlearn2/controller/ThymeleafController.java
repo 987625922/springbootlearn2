d8cb56c8e0890c7d3148bc465b291ba8b88187e1
98762 <987625922@qq.com>
2019-11-18 23:10:26 +0800

    thymeleaf的学习，动态设置页面的值

--- src/main/java/com/wind/springbootlearn2/controller/ThymeleafController.java
+++ src/main/java/com/wind/springbootlearn2/controller/ThymeleafController.java
@@ -1,14 +1,35 @@
 package com.wind.springbootlearn2.controller;
 
+import com.sun.deploy.net.HttpResponse;
+import com.wind.springbootlearn2.entity.TestSetting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/**
+ * thymeleaf的Controller页面
+ * 主要学如何使用thymeleaf
+ */
 @Controller
 @RequestMapping("/thymeleaf")
 public class ThymeleafController {
 
+    @Autowired
+    private TestSetting testSetting;
+
+    /**
+     * 使用thymeleaf
+     * 直接打开templates资源目录下的index.html网页
+     */
     @RequestMapping("hello")
     public String hello() {
         return "index";
+    }
+
+
+    @RequestMapping("setValue")
+    public String setValue(ModelMap modelMap){
+        modelMap.addAttribute("value",testSetting);
+        return "admin/info";
     }
 }
