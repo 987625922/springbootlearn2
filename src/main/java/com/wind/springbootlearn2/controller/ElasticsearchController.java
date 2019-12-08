0e6785b63213f395a3f13365734a798150b7eb39
98762 <987625922@qq.com>
2019-12-08 14:38:50 +0800

    elasticsearch搜索功能

--- src/main/java/com/wind/springbootlearn2/controller/ElasticsearchController.java
+++ src/main/java/com/wind/springbootlearn2/controller/ElasticsearchController.java
@@ -3,6 +3,8 @@ package com.wind.springbootlearn2.controller;
 import com.wind.springbootlearn2.domain.JsonData;
 import com.wind.springbootlearn2.domain.Article;
 import com.wind.springbootlearn2.repository.ArticleRepository;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +22,32 @@ public class ElasticsearchController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    /**
+     * 把数据保存到elasticsearch中提供搜索
+     *
+     */
     @GetMapping("save")
     public Object save() {
         Article article = new Article();
-        article.setId(System.currentTimeMillis());
+        article.setId(1l);
         article.setPv(888);
         article.setContent("这是内容");
         article.setTitle("这是标题");
         article.setSummary("概要搜索");
         articleRepository.save(article);
         return new JsonData(200, "", "elastic保存成功！");
+    }
+
+    /**
+     * 搜索elasticsearch中的内容
+     *
+     * @param title 要搜索的标题内容
+     */
+    @GetMapping("search")
+    public Object search(String title){
+//        QueryBuilder queryBuilder = QueryBuilders.matchAllQuery();
+        QueryBuilder queryBuilder = QueryBuilders.matchQuery("title",title);
+        Iterable<Article> list = articleRepository.search(queryBuilder);
+        return new JsonData(200,list,"elastic搜索成功");
     }
 }
